e7e1e42f05742a5ea1e2a2a5ce726dd7fafcfed6
Lucas Siviglia <siviglialucas@gmail.com>
2024-09-02 13:19:02 +1000

    adding router

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
+import { FaMapMarker } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setshowFullDescription] = useState(false);
@@ -30,14 +32,14 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
-          <a
-            href={`/job/${job.id}`}
+          <Link
+            to={`/job/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm" >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
